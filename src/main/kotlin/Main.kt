3cda8bf14e7473de827ece4f325b03cd9e12da06
Andrei Cravtov <the.andrei.cravtov@gmail.com>
2024-02-23 06:25:53 +0000

    added stress testing. but its too late now RIP :/

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,7 @@
 package engineering.fs
 
 import java.text.DecimalFormat
+import kotlin.math.max
 import kotlin.math.sqrt
 import kotlin.random.Random
 
@@ -8,25 +9,45 @@ infix fun DecimalFormat.f(d: Double) = format(d)
 infix fun Int.closest(n: Int) = this / n * n
 
 fun testDiv(source: String,
-            iterations: Int = 100_000,
-            minQuotient: Int = 500,
+            stressIterations: Int = 1_000_000,
+            dataIterations: Int = 100_000,
+            minQuotient: Int = 20_000,
             groupSize: Int = 20,
             barSize: Int = 150) {
     // Load program
     val program = parseTokens(parseString(source))
     val cpu = CPU(program)
 
-    // Testing and diagnostics
+    // Stress testing and diagnostics
+    repeat(stressIterations) {
+        val dividend = Random.nextInt(0, 0xFFFF + 1)
+        val divisor = Random.nextInt(1, 0xFFFF + 1)
+        val quotient = ((dividend.toLong() and 0xFFFF) / (divisor.toLong() and 0xFFFF)).toInt()
+        val remainder = if (dividend == 0) 0 else ((dividend.toLong() and 0xFFFF) % (divisor.toLong() and 0xFFFF)).toInt()
+
+        when (val result = cpu.run(listOf(dividend,divisor,0,0,0,0,0,0,0,0,0,0,0,0,0,0))) {
+            is RunResult.Error -> throw RuntimeException("Failed to compute correct result. Expected $dividend÷$divisor=($quotient rem $remainder); got runtime error ${result.reason}")
+            is RunResult.Ok -> {
+                val cQuot = result.registers[2]
+                val cRem = result.registers[3]
+                if (quotient != cQuot || remainder != cRem)
+                    throw RuntimeException("Failed to compute correct result. Expected $dividend÷$divisor=($quotient rem $remainder); got ($cQuot rem $cRem)")
+            }
+        }
+    }
+
+    // Data collection
     val rawData = mutableListOf<Int>()
-    while (rawData.size < iterations) { // number of iterations
+    val minDividend = max(1, minQuotient)
+    while (rawData.size < dataIterations) { // number of iterations
         // generate model solution
-        val dividend = Random.nextInt(minQuotient, 0xFFFF + 1)
-        val divisor = Random.nextInt(1, dividend / minQuotient + 1)
+        val dividend = Random.nextInt(minDividend, 0xFFFF + 1)
+        val divisor = Random.nextInt(1, dividend / minDividend + 1)
         val quotient = ((dividend.toLong() and 0xFFFF) / (divisor.toLong() and 0xFFFF)).toInt()
         val remainder = ((dividend.toLong() and 0xFFFF) % (divisor.toLong() and 0xFFFF)).toInt()
 
         when (val result = cpu.run(listOf(dividend,divisor,0,0,0,0,0,0,0,0,0,0,0,0,0,0))) {
-            is RunResult.Error -> throw RuntimeException("Runtime error occurred ${result.reason}")
+            is RunResult.Error -> throw RuntimeException("Failed to compute correct result. Expected $dividend÷$divisor=($quotient rem $remainder); got runtime error ${result.reason}")
             is RunResult.Ok -> {
                 val cQuot = result.registers[2]
                 val cRem = result.registers[3]
